5566e37fac4cdafaf9e40c0c89022635564cfde1
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-09 16:59:34 +0100

    event bus country details mvp

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,36 @@
   <div>
     <h1>Countries</h1>
     <div class="main-container">
+      <country-detail :country="selectedCountry"></country-detail>
       <countries-list :countries="countries"></countries-list>
     </div>
   </div>
 </template>
 
 <script>
+import { eventBus } from './main.js'
 import CountriesList from './components/CountriesList.vue';
+import CountryDetail from './components/CountryDetail.vue';
 export default {
   name: 'app',
   data(){
     return {
       countries: [],
-      selectedCountrie: null
+      selectedCountry: null
     }
   },
   mounted(){
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(countries => this.countries = countries);
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country;
+    })
   },
   components: {
-    "countries-list": CountriesList
+    "countries-list": CountriesList,
+    "country-detail":CountryDetail
   }
 }
 </script>
