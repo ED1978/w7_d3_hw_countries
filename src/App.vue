cc1016a5f981de70d8d58d877f6590dad8c0cf15
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-09 15:58:24 +0100

    countries fetched

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template lang="html">
-
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries="countries"></countries-list>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountries: null
+    }
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries);
+  }
 }
 </script>
 
