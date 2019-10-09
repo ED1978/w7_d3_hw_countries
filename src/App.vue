ecd145900a55bdac105717db7e1ced71ab3de10a
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-09 16:32:07 +0100

    displaying country names

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,22 @@
 </template>
 
 <script>
+import CountriesList from './components/CountriesList.vue';
 export default {
   name: 'app',
   data(){
     return {
       countries: [],
-      selectedCountries: null
+      selectedCountrie: null
     }
   },
   mounted(){
     fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(countries => this.countries = countries);
+  },
+  components: {
+    "countries-list": CountriesList
   }
 }
 </script>
