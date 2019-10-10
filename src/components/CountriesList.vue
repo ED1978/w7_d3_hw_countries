ba13114b3fa260e25f61f4b0cd014a7d7333501c
EDownie <53910027+ED1978@users.noreply.github.com>
2019-10-10 10:01:37 +0100

    dropdowns working

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,9 +1,10 @@
 <template lang="html">
   <div>
     <label for="select-country">Select a country to view its details: </label>
-    <select id="select-country">
-      <option :value="country" v-for="country in countries">{{country.name}}</option>
+    <select id="select-country" v-model="country" v-on:change="handleChange">
+      <option v-for="country in countries" :value="country">{{country.name}}</option>
     </select>
+
     <!-- <ul>
       <list-item v-for="(country, index) in countries" :country="country" :key="index"></list-item>
     </ul> -->
@@ -11,10 +12,21 @@
 </template>
 
 <script>
+import { eventBus } from '../main.js'
 import ListItem from './ListItem.vue'
 export default {
   name: 'countries-list',
-  props: ['countries'],
+  props: ['countries', 'country'],
+  data(){
+    return{
+      country: null
+    }
+  },
+  methods: {
+    handleChange(){
+      eventBus.$emit('country-selected', this.country)
+    }
+  },
   components: {
     "list-item": ListItem
   }
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-<li v-on:click="handleClick">{{country.name}}</li>
+<li>{{country.name}}</li>
 </template>
 
 <script>
@@ -7,11 +7,11 @@ import { eventBus } from '../main.js'
 export default {
   name: 'list-item',
   props: ['country'],
-  methods: {
-    handleClick(){
-      eventBus.$emit('country-selected', this.country)
-    }
-  }
+  // methods: {
+  //   handleChange(){
+  //     eventBus.$emit('country-selected', this.country)
+  //   }
+  // }
 }
 </script>
 
